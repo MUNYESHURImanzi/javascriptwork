a9fcdeb6c97e423d0f5a7f079607d735ffcfa862
Munyeshuri Manzi Patrick <munyeshurimanzi@gmail.com>
2023-04-12 04:13:14 -0700

    my update

---
+++ index.js
@@ -0,0 +1,118 @@
+// //check number is possitive or negative
+// let a
+// if (a<0) {
+//     console.log("the number is negative")
+    
+// }
+// if (a>0) {
+//     console.log("the number is positive")
+    
+// }
+// else{
+//    console.log("invailid") 
+// }
+
+// //sum of all numbers 
+
+// let a2;
+// let n2;
+// let sum=0;
+// a2=1
+// while(a<n2){
+// sum=sum+a2
+// }
+// a2++
+// console.log('the sum of all number is'+sum )
+
+//promp that tells the user password is invalid until it became collect
+var pass="munyeshuri@1"
+var pa=prompt("enter password");
+do {
+    if (pa===pass) {
+        console.log("your password is correct")
+        
+    }
+    else{
+        console.log("password is invalid")
+    }
+} while (true);
+
+
+
+
+//prints all even numbers from 1 to 20 using a loop.
+
+// let even;
+// for(even=0;even<20;even++){
+//     if (even%2===0) {
+//         console.log("the even is " +even)
+        
+//     }
+
+// }
+
+// //checks if a given year is a leap year or not using an if condition.
+
+// let year;
+
+// if (year%4==0) {
+//     console.log("the year is leap year")
+    
+// }
+// if (year%4==1) {
+//     console.log("the year is not leap year")
+    
+// }
+// else{
+//   console.log("invailid")  
+// }
+
+// //factoriol of a given number 
+
+// let fact;
+// let x;
+// let n;
+// for (x=0;x<n;x++) {
+//     fact=fact*x;
+
+    
+    
+// }
+// console.log("factorio is" +fact)
+
+
+// //a number and prints the multiplication table of that number using a while loop.
+
+// let mult;
+// let m;
+// m=1
+// while (m<12) {
+//     mult=mult*m;
+//     console.log(mult)
+    
+// }
+// m++
+
+
+//to guess a number and sow that is higher or lower
+
+// var guessn=66;
+// var number=prompt("guess the number you wish")
+
+
+
+// // Fibonacci sequence up to a given number using for a loop.
+
+// let e=0;
+// let j;
+// let f=1;
+// let g=25
+// let fibo;
+// for (j=0;j<g;j++) {
+
+//     fibo=e+f;
+//     e=f;
+//     e=fibo;
+//     console.log("fib is"+fibo)
+
+// }
